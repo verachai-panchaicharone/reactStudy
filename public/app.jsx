bc801703be20a7763737310585349dc542972bee
Verachai Panchaicharone <petersaint2000@gmail.com>
2017-03-07 16:45:48 +0700

    Webpack: Custom Package Names

--- public/app.jsx
+++ public/app.jsx
@@ -1,12 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Greeter = require('./components/Greeter')
+var Greeter = require('Greeter')
 
 
-var firstName = "Sawasdee";
+var firstName = "Jen";
 var messageProp = "Message Ja.";
 
 ReactDOM.render(
-    <Greeter name={firstName} message={messageProp} />, 
+    <Greeter name={firstName} message={messageProp} />,
     document.getElementById('app')
   );
