73d9dbc550cd85a914315a336690a5adeaca40e8
Verachai Panchaicharone <petersaint2000@gmail.com>
2017-03-07 15:53:54 +0700

    Adding Babel & JSX Support

--- public/app.jsx
+++ public/app.jsx
@@ -1,11 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
 var GreeterMessage = React.createClass({
     render: function() {
         var name = this.props.name;
         var message = this.props.message;
         return (
             <div>
-                <h1>Hello {name}
-                    !!</h1>
+                <h1>Hello {name} !!</h1>
                 <p>{message}</p>
             </div>
         );
